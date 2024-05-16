f07b5c6b6361daecbe18c54be76ef6f28cb2adb7
brunoedcf <brunoesteves.dcf@outlook.com>
2024-05-16 17:46:54 -0300

    Disable selenium logging

--- scraper/utils.py
+++ scraper/utils.py
@@ -1,4 +1,5 @@
 import time
+import logging
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -7,18 +8,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.remote.remote_connection import LOGGER as selenium_logger
+
+selenium_logger.setLevel(logging.WARNING)
 
 """Download the HTML of a web page using Selenium"""
 def fetch_page(url, element_id):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--log-level=3")
+    chrome_options.add_argument("--disable-logging")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+
     service = ChromeService()
     driver = webdriver.Chrome(service=service, options=chrome_options)
-    driver.get(url)
-    time.sleep(2)
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, element_id)))
-    html = driver.page_source
-    driver.quit()
+
+    try:
+        driver.get(url)
+        time.sleep(2)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, element_id)))
+        html = driver.page_source
+
+    finally:
+        driver.quit()
 
     return html
 
