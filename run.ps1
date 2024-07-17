49f5090c362be8e4a496cf4da7ae5e0556c9fe48
brunoedcf <brest.dallacosta@outlook.com>
2024-07-17 15:11:42 -0300

    update run.ps1. adding a way to stop chrome.exe processes and clear CPU usage.

--- run.ps1
+++ run.ps1
@@ -1,19 +1,37 @@
 Set-Location -Path $PSScriptRoot
 
-if (-Not (Test-Path "venv")) {
-    python -m venv venv
-    Write-Output "Virtual environment created."
+while ($true) {
+    if (-Not (Test-Path "venv")) {
+        python -m venv venv
+        Write-Output "Virtual environment created."
 
-    .\venv\Scripts\Activate.ps1
-    Write-Output "Virtual environment activated."
+        .\venv\Scripts\Activate.ps1
+        Write-Output "Virtual environment activated."
 
-    pip install -r requirements.txt
-    Write-Output "Dependencies installed."
+        pip install -r requirements.txt
+        Write-Output "Dependencies installed."
+    }
+    else {
+        .\venv\Scripts\Activate.ps1
+        Write-Output "Virtual environment activated."
+    }
+
+    try {
+        python main.py
+    }
+    finally {
+        Write-Output "Cleaning up processes..."
+
+        # Find and stop ChromeDriver processes
+        $chromeDriverProcesses = Get-Process | Where-Object { $_.ProcessName -eq "chrome" }
+        foreach ($process in $chromeDriverProcesses) {
+            Stop-Process -Id $process.Id -Force
+        }
+
+        # Deactivate the virtual environment
+        deactivate
+    }
+
+    Write-Output "Waiting for 30 minutes before next run..."
+    Start-Sleep -Seconds 1800
 }
-else {
-    .\venv\Scripts\Activate.ps1
-    Write-Output "Virtual environment activated."
-}
-
-python main.py
-deactivate
